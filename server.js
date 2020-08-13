3f33c73ef98a38529cc8d8a3ad2dd9695642f04d
Leon Letto <leon.letto@gmail.com>
2020-08-12 18:56:40 -0700

    Update API

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const https = require('follow-redirects').https;
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
-const privateKey = fs.readFileSync('ca/privatekeys/localhost.key', 'utf8');
-const certificate = fs.readFileSync('ca/certs/localhost.crt', 'utf8');
-const credentials = {key: privateKey, cert: certificate};
-
-
 const app = express();
+const Agent = require('agentkeepalive');
+
+const keepAliveAgent = new Agent({
+  maxSockets: 40,
+  maxFreeSockets: 10,
+  timeout: 600000, // active socket keepalive for 60 seconds
+  freeSocketTimeout: 300000, // free socket keepalive for 30 seconds
+});
+
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 const port = process.env.PORT || 4444;
 
 const queryBuilder = (queries1) => {
@@ -25,7 +31,8 @@ const queryBuilder = (queries1) => {
 };
 
 
-const requestBuilder = (param1, param2, param3, param4, param5, queries) => {
+const requestBuilder2 = (params, queries) => {
+
   let myqueries = queryBuilder(queries);
   let query;
   if (myqueries.length > 1) {
@@ -33,96 +40,86 @@ const requestBuilder = (param1, param2, param3, param4, param5, queries) => {
   } else {
     query = '';
   }
-
-  if (
-    param1.length > 1 &&
-    param2.length > 1 &&
-    param3.length > 1 &&
-    param4.length > 1 &&
-    param5.length > 1
-  ) {
-    return (
-      '/api/' +
-      param1 +
-      '/' +
-      param2 +
-      '/' +
-      param3 +
-      '/' +
-      param4 +
-      '/' +
-      param5 +
-      query
-    );
-  } else if (
-    param1.length > 1 &&
-    param2.length > 1 &&
-    param3.length > 1 &&
-    param4.length > 1 &&
-    param5.length === 1
-  ) {
-    return (
-      '/api/' + param1 + '/' + param2 + '/' + param3 + '/' + param4 + query
-    );
-  } else if (
-    param1.length > 1 &&
-    param2.length > 1 &&
-    param3.length > 1 &&
-    param4.length === 1 &&
-    param5.length === 1
-  ) {
-    return '/api/' + param1 + '/' + param2 + '/' + param3 + query;
-  } else if (
-    param1.length > 1 &&
-    param2.length === 1 &&
-    param3.length === 1 &&
-    param4.length === 1 &&
-    param5.length === 1
-  ) {
-    return '/api/' + param1 + '/' + param2 + query;
+  let paramstring = '/api';
+  for (let i = 0; i < params.length; i++) {
+    paramstring = paramstring + '/' + params[i];
   }
+  return paramstring + query;
 };
 
 app.get('/wsoneapi', async (req, res0) => {
-  const {authorization, domain, param1, param2, param3, param4, param5} = req.headers;
+  const {
+    authorization,
+    domain,
+    path,
+    username,
+  } = req.headers;
   const awtenantcode = req.headers['aw-tenant-code'];
   if (!authorization) {
     console.log('error: You are not authorized!');
     return res0.status(401).send({error: 'You are not authorized!'});
-  } else if (!domain) {
-    return res0.status(400).send({
-      error: 'You forgot to include the environment domain name!',
-    });
   }
+  let start = Date.now();
+  let ip =
+    (req.headers['x-forwarded-for'] || '')
+      .split(',')
+      .pop()
+      .trim() ||
+    req.connection.remoteAddress ||
+    (req.connection && req.connection.remoteAddress) ||
+    req.socket.remoteAddress ||
+    (req.socket && req.socket.remoteAddress) ||
+    req.connection.socket.remoteAddress ||
+    undefined;
 
   let options = {
-    method: 'GET',
-    hostname: domain,
-    path: requestBuilder(param1, param2, param3, param4, param5, req.query),
-    headers: {
-      Authorization: authorization,
+    keepAliveAgent: keepAliveAgent,
+    'method': 'GET',
+    'hostname': domain,
+    path: requestBuilder2(
+      JSON.parse(path),
+      req.query
+    ),
+    'headers': {
+      'Authorization': authorization,
       'aw-tenant-code': awtenantcode,
-      Accept: 'application/json;version=2;',
-      Cookie: '__cfduid=deed201afd27e50d8dc45ea9a40b913f91587694655'
+      'Accept': 'application/json;version=2;',
+      'Cookie': '__cfduid=deed201afd27e50d8dc45ea9a40b913f91587694655'
     },
-    maxRedirects: 20
+    'maxRedirects': 20
   };
-  https
-    .request(options, function (res) {
-      const chunks = [];
-      const ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress;
-
-      console.log(ip + ' ' + options.hostname + options.path);
+  try {
+    https.request(options, function (res) {
+      let chunks = [];
 
       res.on('data', function (chunk) {
         chunks.push(chunk);
       });
 
       res.on('end', function () {
-        const body = Buffer.concat(chunks);
+        var body = Buffer.concat(chunks);
+        if (options.path.split('/')[3] === 'admins') {
+          let adminJson;
+          let admins;
+          try {
+            adminJson = JSON.parse(body.toString());
+            admins = adminJson['Admins'];
+          } catch (e) {
+            console.log(e); // error in the above string (in this case, yes)!
+          }
+          if (admins) {
+            for (let i = 0; i < admins.length; i++) {
+              if (admins[i]['UserName'] === username) {
+                console.log(
+                  `admin user ${admins[i]['UserName']} from Org: ${admins[i]['LocationGroup']} in env ${options.hostname} logged in.`
+                );
+              }
+            }
+          }
+
+        }
+        let restime = Date.now() - start;
+        console.log(ip + ' ' + options.hostname + options.path + ' ResponseTime:' + restime + 'ms');
         res0.send(body);
       });
 
@@ -130,12 +127,26 @@ app.get('/wsoneapi', async (req, res0) => {
         console.error(error);
       });
     }).end();
-
+  } catch (e) {
+    console.log(e);
+  }
 });
 
-app.use(serveStatic(path.join(__dirname, 'www'), {'index': ['index.html']}));
+app.use(serveStatic(path.join(__dirname, 'src/pages'), {'index': ['index.html']}));
 
-var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(port, () =>{
-  console.log('Listening on https://localhost:' + port + '/');
-});
+try {
+  if (fs.existsSync('ca/privatekeys/localhost.key')) {
+    const privateKey = fs.readFileSync('ca/privatekeys/localhost.key', 'utf8');
+    const certificate = fs.readFileSync('ca/certs/localhost.crt', 'utf8');
+    const credentials = {key: privateKey, cert: certificate};
+    let httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(port, () => {
+      console.log('Listening on https://localhost:' + port + '/');
+    });
+  } else {
+    console.log(`You must run the createCa.sh script to create your keys.
+    Remember to use a hostname which matches the computer you are hosting this on.`);
+  }
+} catch (err) {
+  console.error(err);
+}
